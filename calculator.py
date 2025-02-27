687506938dce132234ae93d58a0d265402e836a2
ab22059 <ab22059@shibaura-it.ac.jp>
2025-02-27 11:05:02 +0900

    cteate sub

--- calculator.py
+++ calculator.py
@@ -3,9 +3,19 @@ class Calculator:
     def add(a,b):
         return a + b
 
+    @staticmethod
+    def sub(a,b):
+        return a - b
+    
+    @staticmethod
+    def mul(a,b):
+        return a * b
+
+
 if __name__ =="__main__":
     numa = 4
     numb = 2
 
     print(Calculator.add(numa, numb))
-
+    print(Calculator.sub(numa, numb))
+    print(Calculator.mul(numa, numb))
